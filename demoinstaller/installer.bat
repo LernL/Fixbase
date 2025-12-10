e0680262479c387cd6c12fa93c8b837b6566407f
Adm <lernandlord@gmail.com>
2025-12-10 09:44:05 +0200

    first_workflow

--- demoinstaller/installer.bat
+++ demoinstaller/installer.bat
@@ -1,19 +1,13 @@
 @echo off
-REM Ця команда гарантує, що скрипт виконується саме з папки demoinstaller
 cd /d "%~dp0"
 
 echo Building fixbase.exe...
 
-REM Запуск PyInstaller.
-REM "..\code\fixbase.py" вказує шукати файл у сусідній папці.
-REM --distpath . змушує покласти готовий .exe прямо сюди (у demoinstaller), а не створювати папку dist.
-
-pyinstaller --noconsole --onefile --distpath . "..\code\fixbase.py"
+pyinstaller --noconsole --onefile --distpath "%~dp0.." "..\code\fixbase.py"
 
 echo.
 echo Build complete!
 
-REM (Опціонально) Очищення сміття після збірки
 echo Cleaning up temporary files...
 if exist build rmdir /s /q build
 if exist __pycache__ rmdir /s /q __pycache__
